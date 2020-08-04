20bc66a0edc1fe94d5a77691747830093508c577
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-08-04 21:57:39 +0900

    修正しました

--- main.js
+++ main.js
@@ -13,13 +13,10 @@
       let task = {
          id: idNum,
          comment: input.value,
-
-         // conditionWorking: '作業中',
-         // conditionDelete: '削除',
       };
 
       tasks.push(task);
-      //結果ブラウザ上に表示させる
+      //結果をブラウザ上に表示させる
       displayTasks();
    };
 
@@ -36,12 +33,12 @@
          const idTd = document.createElement('td');
          const commentTd = document.createElement('td');
          const conditionWorkingTd = document.createElement('td');
-         const conditionCompleteTd = document.createElement('td'); //¥NEW
+         const conditionCompleteTd = document.createElement('td');
          const conditionDeleteTd = document.createElement('td');
-         conditionWorkingTd.classList.add('btn');
-         conditionCompleteTd.classList.add('btn', 'hidden'); //¥NEW
-         conditionDeleteTd.classList.add('btn', 'delete');
-         // const getRemoveBtn = document.getElementsByClassName('delete'); //¥NEW
+         const buttonTabForWorking = document.createElement('button'); //-NEW
+         const buttonTabForComplete = document.createElement('button'); //-NEW
+         const buttonTabForDelete = document.createElement('button'); //-NEW
+         conditionCompleteTd.classList.add('hidden');
 
          //¥削除機能:削除ボタンが押された時に押された要素を削除する
          conditionDeleteTd.addEventListener('click', () => {
@@ -61,21 +58,26 @@
             conditionCompleteTd.classList.add('hidden');
          });
 
+         //-DOM操作＿描画
          addTaskTarget.appendChild(tr);
          tr.appendChild(idTd); // 1つめ
-         // idTd.textContent = tasks[index].id;
          idTd.textContent = index;
+
          tr.appendChild(commentTd); // 2つ目
          commentTd.textContent = tasks[index].comment; // タスク入力値
-         tr.appendChild(conditionWorkingTd); // 3つ目
-         conditionWorkingTd.textContent = '作業中';
-         //! ※2つ目の3つ目- .hiddenで初期状態では不可視
-         tr.appendChild(conditionCompleteTd);// 3つ目※
-         conditionCompleteTd.textContent = '完了';
-         tr.appendChild(conditionDeleteTd); // 4つ目
-         conditionDeleteTd.textContent = '削除';
 
-         const numOfTasks = addTaskTarget.childElementCount;
+         tr.appendChild(conditionWorkingTd); // 3つ目
+         conditionWorkingTd.appendChild(buttonTabForWorking); //-New
+         buttonTabForWorking.textContent = '作業中'; //-New
+
+         //! ※2つ目の3つ目- .hiddenで初期状態では不可視
+         tr.appendChild(conditionCompleteTd); // 3つ目※
+         conditionCompleteTd.appendChild(buttonTabForComplete); //-New
+         buttonTabForComplete.textContent = '完了'; //-New
+
+         tr.appendChild(conditionDeleteTd); // 4つ目
+         conditionDeleteTd.appendChild(buttonTabForDelete);
+         buttonTabForDelete.textContent = '削除';
       });
    };
 
